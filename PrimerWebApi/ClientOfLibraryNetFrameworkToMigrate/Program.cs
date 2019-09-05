a5b96f50b0f788781a8c60fc45bfbc683bb65a48
Daniel Nogales <40609267+DanielNogales@users.noreply.github.com>
2019-09-05 17:07:01 -0300

    Biblioteca y Cliente, en .NET Framework 4.6.1

---
+++ PrimerWebApi/ClientOfLibraryNetFrameworkToMigrate/Program.cs
@@ -0,0 +1,21 @@
+﻿using LibraryNetFrameworkToMigrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientOfLibraryNetFrameworkToMigrate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //08/18/2018 07:22:16
+            var persona = new Persona() { Id = 100, Nombre = "Daniel", Nacimiento = DateTime.Parse("05/06/1979 00:00:00") };
+
+            Console.WriteLine($"({persona.Id}) {persona.Nombre}, nacido el {persona.Nacimiento}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PrimerWebApi/LibraryNetFrameworkToMigrate/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryNetFrameworkToMigrate
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Nacimiento { get; set; }
+
+        public ICollection<Persona> Hijos { get; set; }
+            = new HashSet<Persona>();
+
+        public Persona Papa { get; set; }
+    }
+}
